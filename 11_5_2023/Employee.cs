386cf1145f872577ade335365395a4b5f5b6e771
Alaina <107879182+Alaina98@users.noreply.github.com>
2023-05-11 19:26:14 +0400

    Day4

---
+++ 11_5_2023/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_5_2023
+{
+    public class Employee
+    {
+        public const double TAX = 0.03;
+        public string Fname;
+        public string lname;
+        public double wage;
+        public double logdHours;
+
+
+        public void Greeting()
+        {
+            Console.WriteLine($"Hello {Fname} {lname}");
+        }
+        public  int addone(int x)
+        {
+            return x + 1;
+
+        }
+    }
+}
---
+++ 11_5_2023/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _11_5_2023
+{
+
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Employee emp=new Employee();
+            Console.WriteLine("Enter employee numbers");
+            int employeeNo = Convert.ToInt32(Console.ReadLine());
+            int[] empNp = new int[employeeNo];
+
+            foreach (int i in empNp)
+            {
+
+
+                //emp_num [] emps= new emps(ReadLine());
+                //for (int i = 0; i < emps.Length; i++)
+
+                Console.WriteLine("Enter your first name: ");
+                emp.Fname = Console.ReadLine();
+                Console.WriteLine("Enter your last name: ");
+                emp.lname = Console.ReadLine();
+                Console.WriteLine("Enter your last wage:");
+                emp.wage = double.Parse(Console.ReadLine());
+                Console.WriteLine("Enter your last logged hours:");
+                emp.logdHours =double.Parse( Console.ReadLine());
+                double total_salary = emp.wage * emp.logdHours;
+                double salary = total_salary - Employee.TAX;
+                Console.WriteLine($"Hello {emp.Fname} {emp.lname} , " +
+                    $"your Salary is {salary}");
+                emp.Greeting();
+                //string gt = emp.Greeting();
+                //cw(gt);
+                // when the type of class is string
+            }
+            int y = 18;
+            emp.addone(y);
+            Console.WriteLine(y);
+
+
+
+
+        }
+    }
+}
