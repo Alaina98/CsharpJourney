d84bc7975f76311c7e3afe379e0dd747f297cd10
Alaina <107879182+Alaina98@users.noreply.github.com>
2023-05-16 13:51:27 +0400

    16/5/2023

---
+++ Inheretance/Animal.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheretance
+{
+    abstract public class Animal
+    {
+        public string name;
+        int age;
+        public virtual void move()
+        {
+            Console.WriteLine("moving");
+        }
+        //Abstract+body of them will be in the child
+        public abstract void makeSound();
+    }
+
+    public class Cat:Animal {
+        public string gender;
+
+        public override void makeSound()
+        {
+            Console.WriteLine("Mewoooo");
+        }
+
+        public override void move() 
+        { 
+            //give the move from animal
+            base.move();
+            Console.WriteLine("Four legs");
+
+        }
+
+        public void sound()
+        {
+            Console.WriteLine("Sound");
+        }
+
+        public override string ToString()
+        {
+            return $"my name is {this.name}";
+        }
+
+    }
+
+    public class Dog:Animal
+    {
+        string cateqory;
+
+        public override void makeSound()
+        {
+            Console.WriteLine("HOW HOW");
+        }
+
+        public void sound() { Console.WriteLine("Sound"); }
+    }
+     sealed public class Eagle : Animal
+    {
+        string gender;
+        public void fly()
+        {
+            Console.WriteLine("flying");
+        }
+
+        public override void makeSound()
+        {
+            Console.WriteLine("WEEEEEE");
+        }
+    }
+
+
+}
---
+++ Inheretance/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Inheretance
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Cat cat = new Cat();
+           cat.name = "semba";
+           Dog dog = new Dog(); 
+           Eagle eagle = new Eagle();
+            Console.WriteLine(cat);
+
+        }
+    }
+}
