6da5289e3e1dbaac30b65631c0df0eed6eebc48a
Alaina <107879182+Alaina98@users.noreply.github.com>
2023-05-21 16:16:50 +0400

    indexer

---
+++ Indexer/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Indexer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           phoneBook b1=new phoneBook(2);
+            b1[1,"a"]="123";
+            b1[2, "b"] = "2344";
+            Console.WriteLine(b1.getValue("b"));
+
+        }
+    }
+}
---
+++ Indexer/phoneBook.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    internal class phoneBook
+    {
+        private string[] name;
+        private string[] phoneNumbers;
+        int size;
+        int counter = 0;
+
+        public phoneBook(int size)
+        {
+            this.size = size;
+            this.name = new string[this.size];
+            this.phoneNumbers = new string[this.size];
+        }
+        public string  this[int idx  , string name] { 
+            get {
+                for (int i = 0; i < size; i++)
+                {
+                    if (this.name[i] == name)
+                    {
+                        return phoneNumbers[i];
+                    }
+
+                }
+                return "Not found";
+            }
+            
+            set {
+                phoneNumbers[idx] = value;
+                this.name[idx] = name;
+            
+            } }
+        public string this[string name]
+        {
+            get
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (this.name[i] == name)
+                    {
+                        return this.phoneNumbers[i];
+                    }
+                }
+                return "notfound";
+            }
+        }
+        public void setValues(int index, string name, string numbers)
+        {
+            //validation
+            if ((index > 0) && (index < size) && (index > counter))
+            {
+                this.phoneNumbers[index] = numbers;
+                this.name[index] = name;
+                counter++;
+            }
+        }
+
+        public string getValue(string name)
+        {
+            //? for check
+            for (int i = 0; i < name?.Length; i++)
+            {
+                if (this.name[i] == name)
+                {
+                    return phoneNumbers[i];
+                }
+
+
+            }
+            return "not found";
+
+        }
+    }
+}
