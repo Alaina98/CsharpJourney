29733a083117f4625c2d4cca2c3f68102de058ad
Alaina <107879182+Alaina98@users.noreply.github.com>
2023-05-23 19:22:38 +0400

    Operator Over Loading

---
+++ OperatorOverLoading/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Data.SqlTypes;
+
+namespace OperatorOverLoading
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Money m1 = new Money(25);
+            Money m2 = new Money(5);
+            Money m3=m1 + m2;
+            Money m4 = m1 *m2;
+            Money m5 = m1 / m2;
+            Console.WriteLine($"Sum : {m3.amount}");
+            Console.WriteLine($"Multiplucation :{m4.amount}");
+            Console.WriteLine($"Divinition {m5.amount}");
+        }
+    }
+
+    
+    public class Money
+    {
+        public double amount;
+        public Money(double amount)
+        {
+            this.amount = amount;
+        }
+       
+
+
+        public static Money operator +(Money m1, Money m2)
+        {
+            double value=m1.amount+m2.amount;
+            return new Money(value);
+           
+
+        }
+        public static Money operator *(Money m1, Money m2)
+        {
+            double value = m1.amount * m2.amount;
+            return new Money(value);
+
+
+        }
+        public static Money operator /(Money m1, Money m2)
+        {
+            double value = m1.amount / m2.amount;
+            return new Money(value);
+
+
+        }
+
+
+    }
+}
+
+
+
+
