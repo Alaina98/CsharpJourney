28e9bf5753b6b8dc49765c7d391cd4caa6248350
Alaina <107879182+Alaina98@users.noreply.github.com>
2023-05-24 15:40:16 +0400

    change weather state using Events

---
+++ Weather_Using_Event/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace EventAss
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            weather w = new weather();
+
+            w.OnChange += Weather_OnChange;
+            w.WeatherChanges(55);
+
+
+
+        }
+
+        private static void Weather_OnChange(weather weath, int temp)
+        {
+            if (temp < 17)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Rainy and Cold");
+            }
+            else if (temp > 17 && temp < 25)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Cloudy and No Rain");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Hot and Melt");
+            }
+        }
+    }
+
+    class weather
+    {
+        public int temperature { get; set; }
+        //public string condition { get; set; }
+        
+        public void WeatherChanges(int temp)
+        {
+            this.temperature = temp;
+
+            //3. event declaration
+            if (OnChange != null)
+            {
+              
+                OnChange(this, temp);
+
+
+            }
+
+        }
+        //1. Delegate
+        public delegate void changingWeatherS(weather weath, int temp);
+        //2. Event
+        public event changingWeatherS OnChange;
+
+    }
+}
