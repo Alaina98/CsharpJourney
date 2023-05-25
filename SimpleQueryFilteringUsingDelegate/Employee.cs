729040f3f1e497dca54ca423d4e9a69c9405bcd2
Alaina <107879182+Alaina98@users.noreply.github.com>
2023-05-25 13:41:31 +0400

    Simple Query Filtering Using Delegate

---
+++ SimpleQueryFilteringUsingDelegate/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Delegante
+{
+    internal class Employee
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public decimal salay { get; set; }
+        public decimal total_salary { get; set; }
+    }
+}
