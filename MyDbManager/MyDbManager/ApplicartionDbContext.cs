3d18bb98778df5714dafb417bfe077fd4ef62d7d
Alaina <107879182+Alaina98@users.noreply.github.com>
2023-06-19 11:56:09 +0400

    Add files via upload

---
+++ MyDbManager/MyDbManager/ApplicartionDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDbManager
+{
+    internal class ApplicartionDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+           // base.OnConfiguring(options);
+            options.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Company;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        DbSet<Employee> employees { get; set; }
+
+    }
+}
---
+++ MyDbManager/MyDbManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MyDbManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
